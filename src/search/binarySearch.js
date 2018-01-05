801a121f2babc3e0210c2304d1d9943b20811326
Daniel James Williams <v-wildan@microsoft.com>
2018-01-05 12:53:37 -0800

    Add binarySearch soltuion/tests.

---
+++ src/search/binarySearch.js
@@ -0,0 +1,37 @@
+
+const solution1 = (a, x) => {
+  function fn(a, x, low, high) {
+    if(low === high) {
+      return a[low] === x;
+    } else {
+      const mid = Math.floor((low + high) / 2);
+
+      if(a[mid] === x) {
+        return true;
+      }
+
+      // Note: remember, we must test if mid is equal to low or high
+      if(a[mid] > x) {
+        return mid === low
+          ? false
+          : fn(a, x, low, mid - 1);
+      } else {
+        return mid === high
+          ? false
+          : fn(a, x, mid + 1, high);
+      }
+    }
+  }
+
+  return a.length
+    ? fn(a, x, 0, a.length - 1)
+    : false;
+}
+
+module.exports = {
+  constants: {
+  },
+  solutions: [
+    solution1,
+  ],
+};
---
+++ src/search/binarySearch.test.js
@@ -0,0 +1,42 @@
+const { constants, solutions } = require('./binarySearch');
+const a = Array(100).fill(0).map((x, i) => i);
+
+
+describe('testing solutions', () => {
+  it('should be an array of solutions', () => {
+    expect(Array.isArray(solutions)).toBe(true);
+  });
+
+  solutions.forEach((x, i) => {
+    describe(`testing solution #${i + 1}`, () => {
+      doTests(x);
+    });
+  });
+});
+
+
+function doTests(fn) {
+  it('should be a function', () => {
+    expect(typeof fn === 'function').toBe(true);
+  });
+
+  it('should find first element', () => {
+    expect(fn(a, a[0])).toBe(true);
+  });
+
+  it('should find last element', () => {
+    expect(fn(a, a[99])).toBe(true);
+  });
+
+  it('should find random values we know exist', () => {
+    const values = Array(10).fill(0).map(x => Math.floor(Math.random() * 100));
+
+    values.forEach(x => expect(fn(a, x)).toBe(true));
+  });
+
+  it('should fail to find values we know do not exist', () => {
+    const values = [-1, -50, 100];
+
+    values.forEach(x => expect(fn(a, x)).toBe(false));
+  });
+}
