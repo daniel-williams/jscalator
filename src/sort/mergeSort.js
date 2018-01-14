614b8fdb563b5ed46097e0b6fdcfce18ce7833fd
Daniel James Williams <v-wildan@microsoft.com>
2018-01-14 13:17:01 -0800

    mergeSort refactor.

--- src/sort/mergeSort.js
+++ src/sort/mergeSort.js
@@ -1,5 +1,35 @@
 // recursively split array into left/right, merge the sorted halves and return
 
+const solution2 = (a) => {
+  const mid = Math.floor(a.length / 2);
+
+  if(mid > 0) {
+    return solution2Merge(solution2(a.slice(0, mid)), solution2(a.slice(mid)));
+  }
+
+  return a;
+}
+
+const solution2Merge = (l, r) => {
+  const len = l.length + r.length;
+  let ri = li = 0;
+  let result = [];
+
+  r.push(Number.POSITIVE_INFINITY);
+  l.push(Number.POSITIVE_INFINITY);
+
+  for(let i = 0; i < len; i++) {
+    if(r[ri] < l[li]) {
+      result.push(r[ri++]);
+    } else {
+      result.push(l[li++]);
+    }
+  }
+
+  return result;
+}
+
+
 const solution1 = (a) => {
   function helper(a) {
     if(a.length <= 1) { return a; }
@@ -8,13 +38,13 @@ const solution1 = (a) => {
     let left = helper(a.slice(0, mid));
     let right = helper(a.slice(mid));
 
-    return mergeArrays(left, right);
+    return solution1Merge(left, right);
   }
 
   return helper(a.slice());
 }
 
-function mergeArrays(a, b) {
+function solution1Merge(a, b) {
   let result = Array(a.length + b.length);
   let ai = 0;
   let bi = 0;
@@ -43,5 +73,6 @@ module.exports = {
   constants: {},
   solutions: [
     solution1,
+    solution2,
   ],
 };
