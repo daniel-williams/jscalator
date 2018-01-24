3dd0ab350fc6c65219dd54a66f1e2af2a5788bb4
Daniel James Williams <mail@danielwilliams.us>
2018-01-23 19:02:37 -0800

    Add primes solutions and tests.

---
+++ src/math/primes.test.js
@@ -0,0 +1,40 @@
+const { constants, solutions } = require('./primes');
+const tests = [
+    [-19, false],
+    [0, false],
+    ['wtf', false],
+    [2, true],
+    [4, false],
+    [5, true],
+    [9, false],
+    [21, false],
+    [157, true],
+    [829, true],
+    [847, false],
+    [15227, true],
+    [15230, false],
+];
+
+describe('testing primes solutions', () => {
+    it('should contain an array of solutions', () => {
+        expect(Array.isArray(solutions)).toBe(true);
+    });
+
+    solutions.forEach((solution, i) => {
+        describe(`testing solution #${i + 1}`, () => {
+            it('should be a function', () => {
+                expect(typeof solution).toEqual('function');
+            });
+
+            doTests(solution);
+        });
+    });
+});
+
+function doTests(fn) {
+    tests.forEach(([num, result]) => {
+        it(`given ${num}, expect result to be ${result}`, () => {
+            expect(fn(num)).toBe(result);
+        });
+    });
+}
