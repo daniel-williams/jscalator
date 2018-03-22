2779b9b6c8062f2e19999c74dc6f177272192792
Daniel James Williams <v-wildan@microsoft.com>
2018-03-22 14:47:49 -0700

    Implementing HashTable.

--- src/structures/hashTable/hashTable.js
+++ src/structures/hashTable/hashTable.js
@@ -1,27 +1,95 @@
 class HashTable {
-  constructor(size) {
-    this._table = new Array(size).fill(undefined);
+  constructor(size = 10) {
+    this._count = 0;
+    this._table = new Array(size).fill(null);
   }
 
-  getHashIndex(key) {
-    let hash = 0;
-
-    for(let i = 0; i < key.length; i += 4) {
-      let val = 0;
-
-      for(let j = 3; j >= 0; j--) {
-        let code = key.charCodeAt(i + j);
-
-        if(!isNaN(code)) {
-          let tmp = code * Math.pow(1000, j);
-          val += tmp;
-        }
-      }
-      hash += val;
+  insert(key, data) {
+    if(this._count > this._table.length * 0.7) {
+      this.expand();
     }
 
-    return hash % this._table.length;
+    let index = this.getIndex(key);
+    let newNode = new HashNode(key, data);
+
+    if(!this._table[index]) {
+      this._table[index] = newNode;
+    } else {
+      let node = this._table[index];
+
+      while(node.next) {
+        node = node.next;
+      }
+
+      node.next = newNode;
+    }
+
+    this._count++;
   }
+
+  delete(key) {
+    let index = this.getIndex(key);
+    let node = this._table[index];
+
+    if(!node) { return; }
+
+    if(node.key === key) {
+      this._table[index] = node.next;
+    } else {
+      while(node.next) {
+        if(node.next.key === key) {
+          node.next = node.next.next;
+        }
+        node = node.next;
+      }
+    }
+  }
+
+  search(key) {
+    let index = this.getIndex(key);
+    let node = this._table[index];
+
+    while(node) {
+      if(node.key === key) {
+        break;
+      }
+
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  expand() {
+    // academic... as JavaScript arrays are already dynamic
+    let oldTable = this._table;
+
+    this._table = new Array(this._table.length * 2).fill(null);
+    this._count = 0;
+
+    for(const item of oldTable) {
+      let node = item;
+
+      while(node) {
+        this.insert(node.key, node.value);
+        node = node.next;
+      }
+    }
+  }
+
+  getIndex(key) {
+    return Math.abs(this.getHash(key)) % this._table.length;
+  }
+
+  getHash(key) {
+    for(var hash = 0, i = 0; i < key.length; i++) {
+      hash = (hash << 5) - hash + key.charCodeAt(i);
+      hash &= hash;
+    }
+
+    return hash;
+  }
+
 }
 
 class HashNode {
@@ -35,4 +103,4 @@ class HashNode {
 
 module.exports = {
   HashTable,
-}
+};
