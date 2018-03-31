cbeb5c5c9937634f6fe7972e04dfcb30d09fcefe
Daniel James Williams <v-wildan@microsoft.com>
2018-03-31 12:51:54 -0700

    Add non recursive bottom-up solution to largestSubmatrix

--- src/puzzles/largestSubmatrix.js
+++ src/puzzles/largestSubmatrix.js
@@ -42,10 +42,9 @@ const A = (arr) => {
 
 const B = (arr) => {
   let max = 0;
-  let cache = [];
-
-  // make cache 2D array to match arr
-  arr[0].forEach((x, i) => cache[i] = []);
+  // init cache based on arr dimensions
+  let cache = new Array(arr.length);
+  arr.forEach((x, i) => cache[i] = new Array(arr[0].length).fill(null));
 
   for(let col = arr[0].length - 1; col >= 0; col--) {
     for(let row = arr.length - 1; row >= 0; row--) {
@@ -53,6 +52,7 @@ const B = (arr) => {
     }
   }
 
+  // printMatrix(cache);
   return max;
 
   function checkSubArray(row, col) {
@@ -75,15 +75,52 @@ const B = (arr) => {
 // Time: O(n * m)
 // Space: O(n * m) --> owing to the cache
 
+const C = (matrix) => {
+  let max = 0;
+  // init cache based on matrix dimensions
+  let cache = new Array(matrix.length);
+  matrix.forEach((x, i) => cache[i] = new Array(matrix[0].length).fill(null));
+
+  for(let col = 0; col < matrix[0].length; col++) {
+    for(let row = 0; row < matrix.length; row++) {
+      // first row & col do not need to check adjacent values
+      if(row === 0 || col === 0) {
+        cache[row][col] = matrix[row][col] ? 1 : 0;
+      } else {
+        if(!matrix[row][col]) {
+          cache[row][col] = 0;
+        } else {
+          cache[row][col] = 1 + Math.min(
+            cache[row-1][col],
+            cache[row][col-1],
+            cache[row-1][col-1],
+          );
+        }
+      }
+      max = Math.max(max, cache[row][col]);
+    }
+  }
+
+  // printMatrix(cache);
+  return max;
+}
+// Time: O(n * m) -> nested for loops
+// Space: O(n * m) -> cache
+
 module.exports = {};
 
 
 // testing
 console.log(`Given the 2D matrix`);
-arr.forEach(row => {
-  let values = [];
-
-  row.forEach(col => values.push(col ? 1 : 0));
-  console.log(`[ ${values.join(', ')}]`);
-});
+printMatrix(arr);
 console.log(`The largest subarray is ${B(arr)}`);
+
+
+function printMatrix(matrix) {
+  matrix.forEach(row => {
+    let values = [];
+
+    row.forEach(col => values.push(col));
+    console.log(`[ ${values.join(', ')}]`);
+  });
+}
