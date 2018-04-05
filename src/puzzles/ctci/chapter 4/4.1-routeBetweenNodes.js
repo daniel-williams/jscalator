6e7c4acd99336810c62c1913b29c043213236539
Daniel James Williams <v-wildan@microsoft.com>
2018-04-04 18:23:12 -0700

    Add minimal solution to routeBetweenNodes

--- src/puzzles/ctci/chapter 4/4.1-routeBetweenNodes.js
+++ src/puzzles/ctci/chapter 4/4.1-routeBetweenNodes.js
@@ -1,6 +1,21 @@
-const { DirectedGraph, DirectedGraphNode } = require('../../../structures');
+const { DirectedGraph, DirectedGraphNode, Stack } = require('../../../structures');
 
-const A = (graph, startNode, endNode) => {
+// Given a directed graph, design an algorithm to find out whether there is a
+// route between two nodes.
+
+// Ideas
+// A) DFS would potentially be more efficient for finding direct routes. Using
+//    a map to track node/parent pairs from start to end would allow us to
+//    return:
+//      a) if the route exists (this scenario)
+//      b) numbers of hops
+//      c) the route itself
+// B) Might be slightly more efficient to use a Set instead of a map and not
+//    worry about tracking the route itself, just focus on finding end node.
+//    Also, an iterative DFS vs recursive DFS might also be more efficient.
+
+
+const A = (startNode, endNode) => {
   if(!startNode || !endNode) { return false; }
 
   let parentMap = {};
@@ -22,17 +37,48 @@ const A = (graph, startNode, endNode) => {
 // Time: O(n) -> worst case if every node is visited
 // Space: O(n) -> call stack & parentMap
 
+const B = (start, end) => {
+  if(!start || !end) { return false; }
+
+  let visited = new Set();
+  let stack = new Stack();
+
+  visited.add(start);
+  stack.push(start);
+
+  while(!stack.isEmpty()) {
+    let node = stack.pop();
+
+    if(node === end) { return true; }
+
+    node.nodes.forEach(n => {
+      if(!visited.has(n.data)) { stack.push(n.data); }
+    });
+  }
+
+  return false;
+}
+// Time: O(n) -> worst case, every node could be visited once
+// Space: O(n) -> Set & Stack
+
 
 const graph = buildTestGraph();
 const nodeCount = graph.nodes.length;
+const solutions = [
+  ['A', A],
+  ['B', B],
+];
 
-for(let i = 0; i < nodeCount; i++) {
-  for(let j = 0; j < nodeCount; j++) {
-    if(i === j) { continue; }
+solutions.forEach(s => {
+  console.log('');
+  for(let i = 0; i < nodeCount; i++) {
+    for(let j = 0; j < nodeCount; j++) {
+      if(i === j) { continue; }
 
-    console.log(`Route between node ${i} and ${j} -> ${A(graph, graph.findNode(i), graph.findNode(j))}`);
+      console.log(`${s[0]}) Route between node ${i} and ${j} -> ${s[1](graph.findNode(i), graph.findNode(j))}`);
+    }
   }
-}
+});
 
 
 
@@ -44,26 +90,16 @@ for(let i = 0; i < nodeCount; i++) {
 
 // help methods for tests
 function buildTestGraph() {
-
-  // create graph
   const graph = new DirectedGraph();
+  const numNodes = 6;
+  const links = [[0,1],[0,4],[0,5],[1,3],[1,4],[2,1],[3,2],[3,4]];
 
-  // create nodes
-  for(let i = 0; i < 6; i++) {
+  // add nodes
+  for(let i = 0; i < numNodes; i++) {
     graph.addNode(new DirectedGraphNode(i));
   }
 
-  // using refs, create links between nodes
-  const links = [
-    [0,1],
-    [0,4],
-    [0,5],
-    [1,3],
-    [1,4],
-    [2,1],
-    [3,2],
-    [3,4],
-  ];
+  // create links between nodes
   for(let i = 0; i < links.length; i++) {
     graph.createLink(links[i][0], links[i][1]);
   }
